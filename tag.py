5a141f2c6bbddf9715ee79891c85961098d5e430
Martín Weingart <mweingart@neuquen.gov.ar>
2017-07-06 10:13:35 -0300

    Uso de libreria mutagen en lugar de eyed3

--- tag.py
+++ tag.py
@@ -1,66 +1,38 @@
-import eyed3
-from datetime import datetime
-
-#  eyed3.log.setLevel("ERROR")
+from mutagen.id3 import ID3, TIT2, \
+                        TDRC, TRCK, \
+                        TPE1, TALB, TCON
 
 
 class Tag():
-    artist = unicode("")
-    genre = unicode("")
-    album = unicode("")
-    title = unicode("")
-    path = unicode("")
-    track_num = 0
-    recording_date = None
-    v22 = False  # si tiene tag v2.2
-    v23 = False  # si tiene tag v2.3
-    v11 = False  # si tiene tag v1.1
+    artist = u""
+    genre = u""
+    album = u""
+    title = u""
+    path = u""
+    track = u""
+    year = u""
 
     # FUNCION QUE CREA UN TAG GENERICO EN BASE A UN ARCHIVO mp3
     def create(self, archivo):
-        print archivo
         self.path = archivo
-        audioID3v2 = eyed3.core.load(archivo, (2, 3, 0))
-        audioID3v1 = eyed3.core.load(archivo, (1, 1, 0))
-
-        # Tiene un tag v2.x
-        if (audioID3v2.tag is not None):
-            # Tiene tag v2.2
-            if (audioID3v2.tag.version == (2, 2, 0)):
-                self.v22 = True
-            # Tiene tag v2.3
-            else:
-                self.v23 = True
-            self.artist = audioID3v2.tag.artist
-            self.album = audioID3v2.tag.album
-            self.title = audioID3v2.tag.title
-            self.genre = audioID3v2.tag.genre
-            self.track_num = audioID3v2.tag.track_num
-            self.recording_date = audioID3v2.tag.recording_date
-
-        # Tiene tag v1.1
-        if (audioID3v1.tag is not None):
-            self.v11 = True
-            if (not(self.v23 or self.v22)):
-                self.recording_date = audioID3v1.tag.release_date
-                self.artist = audioID3v1.tag.artist
-                self.album = audioID3v1.tag.album
-                self.title = audioID3v1.tag.title
-                self.genre = audioID3v1.tag.genre
-                self.track_num = audioID3v1.tag.track_num
+        audio = ID3(self.path)
+        self.artist = unicode(audio.get('TPE1'))
+        self.album = unicode(audio.get('TALB'))
+        self.title = unicode(audio.get('TIT2'))
+        self.genre = unicode(audio.get('TCON'))
+        self.track = unicode(audio.get('TRCK'))
+        self.year = unicode(audio.get('TDRC'))
 
     # FUNCTION QUE ACTUALIZA O  CREA UN TAG EN EL ARCHIVO MP3
     def updateFile(self):
-        audioID3v2 = eyed3.core.load(self.path, (2, 3, 0))
-        if (not self.v23):  # si no tiene un tag v2.3
-            audioID3v2.initTag()
-        audioID3v2.tag.artist = self.artist
-        audioID3v2.tag.album = self.album
-        audioID3v2.tag.recording_date = self.recording_date
-        audioID3v2.tag.title = self.title
-        audioID3v2.tag.genre = self.genre
-        audioID3v2.tag.track_num = self.track_num
-        audioID3v2.tag.save()
+        audio = ID3(self.path)
+        audio.add(TPE1(encoding=3, text=self.artist))
+        audio.add(TALB(encoding=3, text=self.album))
+        audio.add(TDRC(encoding=3, text=self.year))
+        audio.add(TIT2(encoding=3, text=self.title))
+        audio.add(TCON(encoding=3, text=self.genre))
+        audio.add(TRCK(encoding=3, text=self.track))
+        audio.save()
 
     def setGenre(self, genre):
         self.genre = unicode(genre.decode('utf-8'))
@@ -69,7 +41,7 @@ class Tag():
         self.album = unicode(album.decode('utf-8'))
 
     def setYear(self, year):
-        self.recording_date = datetime(year, 1, 1)
+        self.year = str(year)
 
     def setArtist(self, artist):
         self.artist = unicode(artist.decode('utf-8'))
